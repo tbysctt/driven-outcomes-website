5b6faddb22a46db332dd55df8918de41a9c558d5
Toby Scott <toby@tobyscott.dev>
2026-02-23 10:22:04 +1100

    Add ScrollToTop

--- driven-outcomes-playground/src/components/Layout.tsx
+++ driven-outcomes-playground/src/components/Layout.tsx
@@ -3,10 +3,12 @@ import { CursorDot } from "./CursorDot";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { PromotionalPopupTop } from "./PromotionalPopupTop";
+import { ScrollToTop } from "./ScrollToTop";
 
 export function Layout() {
   return (
     <div className="flex min-h-screen flex-col text-neutral-900 antialiased">
+      <ScrollToTop />
       <CursorDot />
       <PromotionalPopupTop />
       <Header />
